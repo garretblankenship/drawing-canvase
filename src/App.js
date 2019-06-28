9ec8ab2b7ffeb3832c6ec897d125b2c9e64b75e2
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-06-28 11:16:53 +1000

    class 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ColourSelector from "./ColourSelector";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome To Your Drawing Pad</h1>
+                <ColourSelector />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/ColourSelector.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+
+class ColourSelector extends Component {
+    static defaultProps = {
+        hex: "#f4424b"
+    }
+
+    state = {
+        hex: this.props.hex
+    }
+
+    onInputChange(event) {
+        console.log(this.state.hex);
+    }
+    
+    render() {
+        const { hex } = this.state;
+
+        return (
+            <input 
+                type="color" 
+                onChange={this.onInputChange}
+                value={hex} 
+            />
+        );
+    }
+}
+
+export default ColourSelector;
